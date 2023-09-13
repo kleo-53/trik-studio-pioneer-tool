7eb636724bad11aefe480cbf1d6c5cbc11c9cd20
Elizaveta Karaseva <liza.1610@mail.ru>
2023-09-13 18:37:02 +0400

    Add directory

--- test/test_uploader.py
+++ test/test_uploader.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import pytest
 import pioneer_uploader
 from unittest.mock import patch
@@ -19,19 +17,6 @@ def test_few_arguments(mock_logging_error):
     with patch('sys.argv', test_args):
         pioneer_uploader.main()
     mock_logging_error.assert_called_once_with("Invalid arguments")
-
-
-@patch("logging.error")
-def test_correct_arguments(mock_logging_error):
-    current_file = Path(__file__).resolve()
-    file = current_file.parent/'test_file.lua'
-    print(file)
-    test_args = ["parameter", file, "127.0.0.1", "8888", "wifi"]
-    with patch('sys.argv', test_args):
-        with pytest.raises(ConnectionResetError):
-            pioneer_uploader.main()
-    # mock_logging_error.assert_called_once_with(ConnectionResetError(10054,
-    # 'An existing connection was forcibly closed by the remote host', None, 10054, None))
 
 
 if __name__ == "__main__":
