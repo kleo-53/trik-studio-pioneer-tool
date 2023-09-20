6202e01526e9e750fd58c8837b134bb41dd194be
Elizaveta Karaseva <liza.1610@mail.ru>
2023-09-20 19:04:34 +0400

    Fix relative import module in test file

--- test/test_uploader.py
+++ test/test_uploader.py
@@ -1,6 +1,9 @@
 import pytest
-import pioneer_uploader
+import repackage
 from unittest.mock import patch
+
+repackage.up()
+import pioneer_uploader
 
 
 @patch("logging.error")
@@ -23,6 +26,14 @@ def test_few_arguments(mock_logging_error):
     with patch("sys.argv", test_args):
         pioneer_uploader.main()
     mock_logging_error.assert_called_once_with("Invalid arguments")
+
+
+@patch("logging.error")
+def test_args_format(mock_logging_error):
+    test_args = ["parameter", "filename.lusa", "127.0.0.1", "8888", "some_mode"]
+    with patch("sys.argv", test_args):
+        pioneer_uploader.main()
+    mock_logging_error.assert_called_once_with("Invalid program file format")
 
 
 if __name__ == "__main__":
