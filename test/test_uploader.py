772af289936eadc9f698f2b280315f515667532f
Elizaveta Karaseva <liza.1610@mail.ru>
2023-09-22 17:43:52 +0400

    Add unit-tests for the tool

---
+++ test/test_uploader.py
@@ -0,0 +1,32 @@
+import pytest
+import repackage
+from unittest.mock import patch
+
+repackage.up()
+import pioneer_uploader
+
+
+@patch("logging.error")
+def test_without_file(mock_logging_error):
+    test_args = [
+        "parameter",
+        "invalid_file_path",
+        "invalid_ip",
+        "invalid_port",
+        "invalid_mode",
+    ]
+    with patch("sys.argv", test_args):
+        pioneer_uploader.main()
+    mock_logging_error.assert_called_once_with("Can not find program file")
+
+
+@patch("logging.error")
+def test_few_arguments(mock_logging_error):
+    test_args = ["less then", "five", "arguments"]
+    with patch("sys.argv", test_args):
+        pioneer_uploader.main()
+    mock_logging_error.assert_called_once_with("Invalid arguments")
+
+
+if __name__ == "__main__":
+    pytest.main()
