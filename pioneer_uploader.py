cf3122c0cf554e9e099db7bc4b253a16f2cf5a0d
Elizaveta Karaseva <liza.1610@mail.ru>
2023-09-26 18:49:39 +0400

    Add error hanging + tests + adjust logging

--- pioneer_uploader.py
+++ pioneer_uploader.py
@@ -4,6 +4,7 @@ Upload generated program to drone
 import os
 import sys
 import time
+import re
 from pioneer_sdk import Pioneer
 import logging
 
@@ -11,27 +12,46 @@ import logging
 def main():
     try:
         file_path, ip, port, mode = sys.argv[1:5]
-    except ValueError:
-        logging.error("Invalid arguments")
-        return
 
-    if not os.path.exists(file_path):
-        logging.error("Can not find program file")
-        return
+        if not os.path.exists(file_path):
+            raise FileNotFoundError("Can not find program file")
 
-    drone = Pioneer(
-        ip=ip,
-        mavlink_port=port,
-        connection_method="udpout" if mode == "wifi" else "serial",
-        logger=True,
-    )
+        ipv4_pattern = (
+            r"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\."
+            r"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+        )
 
-    try:
-        drone.lua_script_upload(file_path)
+        if not re.match(ipv4_pattern, ip):
+            raise ValueError("Invalid IP address")
+
+        if not 0 < int(port) <= 65535:
+            raise ValueError("Invalid port")
+
+        if mode not in ["wifi", "usb"]:
+            raise ValueError("Invalid mode. Choose 'wifi' or 'usb'")
+
+        drone = Pioneer(
+            ip=ip,
+            mavlink_port=port,
+            connection_method="udpout" if mode == "wifi" else "serial",
+            logger=True,
+        )
+
+        if file_path.split(".")[-1] != "lua":
+            file_name = file_path + "/" + file_path.split("/")[-1] + ".lua"
+            drone.lua_script_upload(file_name)
+        else:
+            drone.lua_script_upload(file_path)
+
         time.sleep(2)
         drone.close_connection()
+
     except Exception as e:
         logging.error(e)
+        logging.info(
+            "Please download uploader from "
+            '<a href="https://docs.geoscan.aero/ru/master/programming/trik/trik_main.html"> site</a>'
+        )
         return
 
 
--- test/test_uploader.py
+++ test/test_uploader.py
@@ -1,31 +1,112 @@
 import pytest
 import repackage
+import os
 from unittest.mock import patch
 
 repackage.up()
 import pioneer_uploader
 
 
-@patch("logging.error")
-def test_without_file(mock_logging_error):
-    test_args = [
-        "parameter",
-        "invalid_file_path",
-        "invalid_ip",
-        "invalid_port",
-        "invalid_mode",
-    ]
-    with patch("sys.argv", test_args):
-        pioneer_uploader.main()
-    mock_logging_error.assert_called_once_with("Can not find program file")
+current_directory = os.path.dirname(os.path.abspath(__file__))
+test_file_path = os.path.join(current_directory, "test_file.lua")
 
 
 @patch("logging.error")
 def test_few_arguments(mock_logging_error):
-    test_args = ["less then", "five", "arguments"]
-    with patch("sys.argv", test_args):
-        pioneer_uploader.main()
-    mock_logging_error.assert_called_once_with("Invalid arguments")
+    test_args = [
+        ["less then", "five", "arguments"],
+        ["parameter", "still", "less", "arguments"],
+        [""],
+        ["parameter", "test_file.lua"],
+    ]
+    expected_errors = [
+        "not enough values to unpack (expected 4, got 2)",
+        "not enough values to unpack (expected 4, got 3)",
+        "not enough values to unpack (expected 4, got 0)",
+        "not enough values to unpack (expected 4, got 1)",
+    ]
+    for i in range(len(test_args)):
+        with patch("sys.argv", test_args[i]):
+            pioneer_uploader.main()
+        args, _ = mock_logging_error.call_args
+        assert isinstance(args[0], ValueError)
+        assert str(args[0]) == expected_errors[i]
+
+
+@patch("logging.error")
+def test_missing_file(mock_logging_error):
+    test_args = [
+        [
+            "parameter",
+            "invalid_file_path.aaa",
+            "invalid_ip",
+            "invalid_port",
+            "invalid_mode",
+        ],
+        ["parameter", "some_file.lua", "invalid_ip", "invalid_port", "invalid_mode"],
+        ["parameter", "8888", "invalid_ip", "invalid_port", "invalid_mode"],
+        ["parameter", "some_file.luac", "invalid_ip", "invalid_port", "invalid_mode"],
+    ]
+    for i in range(len(test_args)):
+        with patch("sys.argv", test_args[i]):
+            pioneer_uploader.main()
+        args, _ = mock_logging_error.call_args
+        assert isinstance(args[0], FileNotFoundError)
+        assert str(args[0]) == "Can not find program file"
+
+
+@patch("logging.error")
+def test_invalid_ip(mock_logging_error):
+    test_args = [
+        ["parameter", test_file_path, "127.3.not.ip", "port", "mode"],
+        ["parameter", test_file_path, "127.3.43254.00", "port", "mode"],
+        ["parameter", test_file_path, "8888", "port", "mode"],
+        ["parameter", test_file_path, "serial", "port", "mode"],
+    ]
+    for i in range(len(test_args)):
+        with patch("sys.argv", test_args[i]):
+            pioneer_uploader.main()
+        args, _ = mock_logging_error.call_args
+        assert isinstance(args[0], ValueError)
+        assert str(args[0]) == "Invalid IP address"
+
+
+@patch("logging.error")
+def test_invalid_port(mock_logging_error):
+    test_args = [
+        ["parameter", test_file_path, "127.0.0.1", "43242342", "mode"],
+        ["parameter", test_file_path, "127.0.0.1", "127.0.0.1", "mode"],
+        ["parameter", test_file_path, "127.0.0.1", "udpin", "mode"],
+        ["parameter", test_file_path, "127.0.0.1", "port", "mode"],
+    ]
+    expected_errors = [
+        "Invalid port",
+        "invalid literal for int() with base 10: '127.0.0.1'",
+        "invalid literal for int() with base 10: 'udpin'",
+        "invalid literal for int() with base 10: 'port'",
+    ]
+    for i in range(len(test_args)):
+        with patch("sys.argv", test_args[i]):
+            pioneer_uploader.main()
+        args, _ = mock_logging_error.call_args
+        assert isinstance(args[0], ValueError)
+        assert str(args[0]) == expected_errors[i]
+
+
+@patch("logging.error")
+def test_invalid_mode(mock_logging_error):
+    test_args = [
+        ["parameter", test_file_path, "127.0.0.1", "8888", "8888"],
+        ["parameter", test_file_path, "127.0.0.1", "8888", "udpin"],
+        ["parameter", test_file_path, "127.0.0.1", "8888", "serial"],
+        ["parameter", test_file_path, "127.0.0.1", "8888", "127.0.0.1"],
+    ]
+    for i in range(len(test_args)):
+        with patch("sys.argv", test_args[i]):
+            pioneer_uploader.main()
+        args, _ = mock_logging_error.call_args
+        assert isinstance(args[0], ValueError)
+        assert str(args[0]) == "Invalid mode. Choose 'wifi' or 'usb'"
 
 
 if __name__ == "__main__":
